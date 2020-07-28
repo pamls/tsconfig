2cc26013c7c79e735dbe159926234341194ab5ea
Paul <pamls@users.noreply.github.com>
2020-07-28 11:34:20 -0400

    update dependencies

---
+++ script/lint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ "$#" -eq 1 ]; then
+    readonly PROJECT_PATH=$1
+    eslint --parser-options "{project: '$PROJECT_PATH/tsconfig.json'}" -c ./.eslintrc.json --no-eslintrc "$PROJECT_PATH/src/**/*.ts" "$PROJECT_PATH/test/**/*.ts"
+else
+    echo "Project path is missing"
+    echo "Help: npm run lint -- PROJECT_PATH"
+fi
