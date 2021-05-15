1e617dfc78a74c15a29eb9f205ac9a8ca4b8051b
Paul Molins <pro@paul-molins.fr>
2021-05-14 20:58:22 -0400

    update linter

--- script/lint.sh
+++ script/lint.sh
@@ -2,7 +2,7 @@
 
 if [ "$#" -eq 1 ]; then
     readonly PROJECT_PATH=$1
-    eslint --parser-options "{project: '$PROJECT_PATH/tsconfig.json'}" -c ./.eslintrc.json --no-eslintrc "$PROJECT_PATH/src/**/*.ts" "$PROJECT_PATH/test/**/*.ts"
+    node_modules/.bin/eslint --parser-options "{project: '$PROJECT_PATH/tsconfig.json'}" -c ./.eslintrc.json --no-eslintrc "$PROJECT_PATH/src/**/*.ts" "$PROJECT_PATH/test/**/*.ts"
 else
     echo "Project path is missing"
     echo "Help: npm run lint -- PROJECT_PATH"
